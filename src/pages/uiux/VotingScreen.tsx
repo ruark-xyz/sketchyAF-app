02ef63ed6a9f85e980814e5fedd72f0b90e28a9b
ruarkferreira <1037155+ruarkferreira@users.noreply.github.com>
2025-06-14 13:14:21 +0200

    Updated VotingScreen.tsx

--- src/pages/uiux/VotingScreen.tsx
+++ src/pages/uiux/VotingScreen.tsx
@@ -22,7 +22,7 @@ const MOCK_SUBMISSIONS = [
     id: 1,
     username: 'SketchLord',
     avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
-    drawingUrl: 'https://images.pexels.com/photos/9844927/pexels-photo-9844927.jpeg?auto=compress&cs=tinysrgb&w=600',
+    drawingUrl: 'https://images.pexels.com/photos/8378736/pexels-photo-8378736.jpeg?auto=compress&cs=tinysrgb&w=600',
     hasVoted: false
   },
   {
